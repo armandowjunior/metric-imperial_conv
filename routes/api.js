fb64d7d170079867f4596bd0cecd7cca371e9fe2
unknown <armandowjunior@gmail.com>
2021-04-03 23:03:35 -0300

    first commit

---
+++ routes/api.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
+
+module.exports = function (app) {
+  let convertHandler = new ConvertHandler();
+
+  app.route("/api/convert").get((req, res) => {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+
+    if (!initNum && !initUnit) {
+      res.send("invalid number and unit");
+    } else if (!initNum) {
+      res.send("invalid number");
+    } else if (!initUnit) {
+      res.send("invalid unit");
+    }
+
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let toString = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit
+    );
+
+    if (initNum && initUnit) {
+      // send the response only if initUnit and initNum are valid
+      res.json({
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit,
+        string: toString,
+      });
+    }
+  });
+};
